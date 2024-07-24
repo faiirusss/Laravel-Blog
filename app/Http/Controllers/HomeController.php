6845ecd37f06323ee81ce62c9918f6a387ec1160
faiirusss <87980888+faiirusss@users.noreply.github.com>
2024-07-24 11:43:03 +0700

    update featured post & latest post at homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,8 @@ class HomeController extends Controller
     public function __invoke(Request $request)
     {
         return view('home', [
-            'featuredPosts' => Post::take(3)->get(),
+            'featuredPosts' => Post::published()->featured()->latest('published_at')->take(5)->get(),
+            'latestPosts' => Post::published()->featured()->latest('published_at')->get(),
         ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,56 +1,45 @@
 <x-app-layout>
     @section('hero')
-    <div class="w-full text-center py-32">
-        <h1 class="text-2xl md:text-3xl font-bold text-center lg:text-5xl text-gray-700">
+    <div class="w-full py-32 text-center">
+        <h1 class="text-2xl font-bold text-center text-gray-700 md:text-3xl lg:text-5xl">
             Welcome to <span class="text-yellow-500">&lt;YELO&gt;</span> <span class="text-gray-900"> News</span>
         </h1>
-        <p class="text-gray-500 text-lg mt-1">Best Blog in the universe</p>
-        <a class="px-3 py-2 text-lg text-white bg-gray-800 rounded mt-5 inline-block"
+        <p class="mt-1 text-lg text-gray-500">Best Blog in the universe</p>
+        <a class="inline-block px-3 py-2 mt-5 text-lg text-white bg-gray-800 rounded"
             href="http://127.0.0.1:8000/blog">Start
             Reading</a>
     </div>
     @endsection
 
-    <div class="mb-10">
+    <div class="w-full mb-10">
         <div class="mb-16">
-            <h2 class="mt-16 mb-5 text-3xl text-yellow-500 font-bold">Featured Posts</h2>
+            <h2 class="mt-16 mb-5 text-3xl font-bold text-yellow-500">Featured Posts</h2>
             <div class="w-full">
-                <div class="grid grid-cols-3 gap-10 w-full">
+                <div class="grid w-full grid-cols-3 gap-10">
                     @foreach ($featuredPosts as $post)
-                    <div class="md:col-span-1 col-span-3">
-                        <a href="http://127.0.0.1:8000/blog/laravel-34">
-                            <div>
-                                <img class="w-full rounded-xl"
-                                    src="http://127.0.0.1:8000/storage/3i5uKG05UnvhbORZ3ieDkvtAOL8ss5-metaZXAxNSAoMjIpLnBuZw==-.png">
-                            </div>
-                        </a>
-                        <div class="mt-3">
-                            <div class="flex items-center mb-2">
-                                <a href="http://127.0.0.1:8000/categories/laravel" class="bg-red-600
-                                            text-white
-                                            rounded-xl px-3 py-1 text-sm mr-3">
-                                    Laravel
-                                </a>
-                                <p class="text-gray-500 text-sm">2023-09-05</p>
-                            </div>
-                            <a class="text-xl font-bold text-gray-900">Laravel 10 tutorial feed page #34</a>
-                        </div>
-
+                    <div class="col-span-3 md:col-span-1">
+                        <x-posts.post-card :post="$post" />
                     </div>
                     @endforeach
                 </div>
             </div>
-            <a class="mt-10 block text-center text-lg text-yellow-500 font-semibold"
+            <a class="block mt-10 text-lg font-semibold text-center text-yellow-500"
                 href="http://127.0.0.1:8000/blog">More
                 Posts</a>
         </div>
         <hr>
 
-        <h2 class="mt-16 mb-5 text-3xl text-yellow-500 font-bold">Latest Posts</h2>
+        <h2 class="mt-16 mb-5 text-3xl font-bold text-yellow-500">Latest Posts</h2>
         <div class="w-full mb-5">
-
+            <div class="grid w-full grid-cols-3 gap-10">
+                @foreach ($latestPosts as $post)
+                <div class="col-span-3 md:col-span-1">
+                    <x-posts.post-card :post="$post" />
+                </div>
+                @endforeach
+            </div>
         </div>
-        <a class="mt-10 block text-center text-lg text-yellow-500 font-semibold" href="http://127.0.0.1:8000/blog">More
+        <a class="block mt-10 text-lg font-semibold text-center text-yellow-500" href="http://127.0.0.1:8000/blog">More
             Posts</a>
     </div>
 
