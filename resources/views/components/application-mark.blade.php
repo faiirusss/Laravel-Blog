dcc87dc76ef1e09ce1fa840dab3759954ed49c89
faiirusss <87980888+faiirusss@users.noreply.github.com>
2024-08-04 22:21:03 +0700

    update: name app

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,3 +1,3 @@
 <div class="font-semibold text-gray-800">
-    <span class="text-xl text-yellow-500">&lt;YELO&gt;</span>
+    <span class="text-xl text-yellow-500">Dreamer</span>
 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
     @section('hero')
     <div class="w-full py-32 text-center">
         <h1 class="text-2xl font-bold text-center text-gray-700 md:text-3xl lg:text-5xl">
-            Welcome to <span class="text-yellow-500">&lt;YELO&gt;</span> <span class="text-gray-900"> News</span>
+            Welcome to <span class="text-yellow-500">Dreamer</span> <span class="text-gray-900"> News</span>
         </h1>
         <p class="mt-1 text-lg text-gray-500">Best Blog in the universe</p>
         <a class="inline-block px-3 py-2 mt-5 text-lg text-white bg-gray-800 rounded"
